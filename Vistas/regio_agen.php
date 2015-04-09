da6686ecd6f81a00431498356465698fbb1bdb8b
csr2000 <csr2000@krauserPC>
2015-04-09 13:23:52 -0500

    Estructura

---
+++ Vistas/regio_agen.php
@@ -0,0 +1,25 @@
+<?php
+include ('../Librerias/adodb5/adodb-pager.inc.php');
+include ('../Librerias/adodb5/adodb.inc.php');
+include ('../Modelo/Conexion.php');
+include ('../Modelo/Modelo.php');
+include ('../Controlador/regio_agen_controlador.php');
+include ('header.php');
+?>
+
+<div class="row margen_top1">
+    <div class="col-lg-5 rojo">
+
+        <img src="" alt="..." class="img-circle">
+    </div>
+    <div class="col-lg-5 col-lg-offset-2 rojo">
+
+        <img src="" alt="..." class="img-circle">
+    </div>
+
+</div>
+
+
+<?php
+include ('footer.php')
+?>
